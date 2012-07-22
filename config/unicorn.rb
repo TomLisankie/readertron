723ef33ffe6d9e33fac627178afa0bcfa1011a62
James Somers <jsomers@gmail.com>
2012-07-22 18:44:38 -0400

    Add unicorn config file

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/jsomers/www/readertron"
+pid "/home/jsomers/www/readertron/tmp/pids/unicorn.pid"
+stderr_path "/home/jsomers/www/readertron/unicorn/unicorn.log"
+stdout_path "/home/jsomers/www/readertron/unicorn/unicorn.log"
+
+listen "/tmp/unicorn.todo.sock"
+worker_processes 2
+timeout 30
