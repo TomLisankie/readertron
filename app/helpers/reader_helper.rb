76486542d451b3ce6215270c83d7fb7f0ca10778
James Somers <jsomers@gmail.com>
2012-11-18 22:07:27 -0500

    Remove the contents of sanitized HTML tags

--- app/helpers/reader_helper.rb
+++ app/helpers/reader_helper.rb
@@ -22,7 +22,7 @@ module ReaderHelper
   end
   
   def clean(html)
-    raw(Sanitize.clean(html, Sanitize::Config::RELAXED.merge({elements: Sanitize::Config::RELAXED[:elements] + ["style"]})))
+    raw(Sanitize.clean(html, Sanitize::Config::RELAXED.merge({elements: Sanitize::Config::RELAXED[:elements] + ["style"], remove_contents: true})))
   end
   
   def feed_favicon(feed_id)
