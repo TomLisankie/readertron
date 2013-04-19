fda105957ecefe7364e0c853aa9abffac6728ade
James Somers <jsomers@gmail.com>
2013-04-19 01:58:02 -0400

    Hook up 'Forgot password?' link etc.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,7 @@ Readertron::Application.configure do
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   # config.action_mailer.smtp_settings = {
   #   :address              => "smtp.gmail.com",
   #   :port                 => 587,
