6b2e6413816fb35e67d966b059432cd070acc20a
James Somers <jsomers@gmail.com>
2013-09-09 21:31:50 -0400

    Don't be so dumb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Readertron::Application.config.secret_token = '726fa4f942ebad59b3771bbf090e76c9bb9702e277a3b62019d105c7ea0aef569f7a5373c346cc88c09889087e0fbd2408dd3a6c2e8ff16f073e09d1575b1ae2'
+Readertron::Application.config.secret_token = if Rails.env.development? || Rails.env.test?
+  ('x' * 30)
+else
+  ENV['SECRET_TOKEN']
+end
